3eed79aee16f412c6400f04cd7cc8d2373f2042d
Anton Zhvakin <anton.zhv@gmail.com>
2024-12-26 23:58:45 +0100

    Publish is_charging in the second topic.

--- publish-to-mqtt.py
+++ publish-to-mqtt.py
@@ -5,7 +5,8 @@ import paho.mqtt.client as mqtt
 
 MQTT_BROKER = "homelab"
 MQTT_PORT = 1883
-MQTT_TOPIC = "sensor/mouse/heroloverpc_razer"
+MQTT_BATTERY_LEVEL_TOPIC = "heroloverpc/mouse/battery/level"
+MQTT_IS_CHARGING_TOPIC = "heroloverpc/mouse/battery/is_charging"
 
 UTILITY_PATH = "C:/Users/anton/Projects/my/razer-mouse-battery/build/Release/razer-mouse-battery.exe"
 
@@ -19,26 +20,35 @@ def get_battery_level():
         data = json.loads(result.stdout)
 
         battery_level = data.get("BatteryLevel")
+        is_charging = data.get("IsCharging")
         is_idle = data.get("IsIdle")
         if battery_level is not None and not is_idle:
-            return round(battery_level, 1)
+            return (round(battery_level, 1), is_charging)
 
     except Exception as e:
         print(f"Error: {e}")
 
-    return None
+    return (None, None)
 
-def publish_battery_level(client):
-    battery_level = get_battery_level()
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code != 0:
+        return
+
+    (battery_level, is_charging) = get_battery_level()
     if battery_level is not None:
-        print(f"Publish battery level: {battery_level}")
-        client.publish(MQTT_TOPIC, payload=battery_level)
+        payload="{\"state\": \"on\"}" if is_charging else "{\"state\": \"off\"}"
+        print(f"Publish: level: {battery_level}%, is_charging: {is_charging} {payload}")
+
+        client.publish(MQTT_BATTERY_LEVEL_TOPIC, payload=battery_level)
+        client.publish(MQTT_IS_CHARGING_TOPIC, payload="{\"state\":\"ON\"}" if is_charging else "{\"state\":\"OFF\"}")
+
+    client.disconnect()
 
 def main():
-    client = mqtt.Client()
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.on_connect = on_connect
     client.connect(MQTT_BROKER, MQTT_PORT, 60)
-
-    publish_battery_level(client)
+    client.loop_forever()
 
 if __name__ == "__main__":
     main()
